d5c6700cbd8f881edc25cf0ab7857f77905b53b6
themakgithub <makkimail@gmail.com>
2015-12-17 11:07:35 +0100

    modifs

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
 class Test {
+
+	//@brief return the sum of x and y;
+	public int add (int x, int y){
+		return x+y+1;
+	}
+
 	public static void main(String args[]){
 		System.out.println("Running tests");
-		int x = 5;
-		assert x == 4;
+
+		Test my_test = new Test();
+
+		assert my_test.add(5,10) == 15 : "suppose retourner 15";
+		assert my_test.add(10,20) == 30: "suppose retourner 30" ;
+		assert my_test.add(14,30) == 44: "suppose retourner 44";
 	}
 }
