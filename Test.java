67782e1168d1f247366245f7c59fb0997db51071
El Makki Voundy <makki@makki-mbair.lidil.univ-mrs.fr>
2015-12-18 09:05:28 +0100

    insertion d erreurs dans le fichier

--- Test.java
+++ Test.java
@@ -1,8 +1,13 @@
 class Test {
+    /*Nous voulons ici tester l'implementation de notre fonction sum.
+     
+     On se servira d'__ assert __ pour verifier que notre fonction renvoie bien
+     ce qu'elle est supposee renvoyer.
+    */
 
-	//@brief return the sum of x and y;
+	//@brief renvoie la somme de x et y ?
 	public int add (int x, int y){
-		return x+y+1;
+		return x+y+1; //une erreur dans notre code. sum est suppose renvoyer x+y
 	}
 
 	public static void main(String args[]){
@@ -10,7 +15,8 @@ class Test {
 
 		Test my_test = new Test();
 
-		assert my_test.add(5,10) == 15 : "suppose retourner 15";
+        //suite de test
+		assert my_test.add(5,10) == 15 : "suppose retourner 15"; //on verifie ici si sum(5,10) == 15
 		assert my_test.add(10,20) == 30: "suppose retourner 30" ;
 		assert my_test.add(14,30) == 44: "suppose retourner 44";
 	}
